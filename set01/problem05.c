eacebba2b3fa81bae2ec85bd343e6fd6d9d67e50
impana.r <impanar.bsc23@rvu.edu.in>
2024-01-29 16:53:14 +0530

    test

--- set01/problem05.c
+++ set01/problem05.c
@@ -1,1 +1,35 @@
+#include <stdio.h>
 
+int input()
+{
+    int number;
+    printf("Enter the numbers:");
+    scanf("%d", &number);
+    return number;
+
+}
+
+int compare(int a, int b, int c)
+{
+    if(a>=b && a>=c)
+    return a;
+    else if(b>=c)
+    return b;
+    return c;
+}
+
+void output(int a, int b, int c, int largest)
+{
+    printf("The largest number of %d, %d, %d is %d", a, b, c, largest);
+}
+
+int main()
+{
+    int a, b, c;
+    a=input();
+    b=input();
+    c=input();
+    int large=compare(a, b, c);
+    output(a, b, c, large);
+    return 0;
+}
