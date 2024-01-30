48bcc1b1a3293691e86db7e16040a3cf365efe04
impana.r <impanar.bsc23@rvu.edu.in>
2024-01-30 20:08:31 +0530

    test

---
+++ set01/problem11.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+struct complex
+{
+    float real, imaginary;
+};
+typedef struct complex Complex;
+
+Complex input_complex()
+{
+    Complex n;
+    prinf("Enter the real number:");
+    scanf("%f", &n.real);
+    printf("Enter the imagianry number:");
+    scanf("%f", &n.imaginary);
+    return n;
+}
+
+Complex add_complex(Complex a, Complex b)
+{
+    Complex sum;
+    sum.real=a.real+b.imaginary;
+    sum.imaginary=a.real+b.imaginary;
+    return sum;
+}
+
+void output(Complex a, Complex b, Complex sum)
+{
+    printf("%f+%fi + %f+%fi = %f+%fi", a.real, b.imaginary, a.imaginary, b.real, sum.real, sum.imaginary);
+}
+
+int main()
+{
+    Complex a, b;
+    a=input_complex();
+    b=input_complex();
+    Complex sum= add_complex(a, b);
+    output(a, b, sum);
+    return 0;
+}
