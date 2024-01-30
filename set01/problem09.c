39e7d68962628f270e32fe21fcf2ee264185cd18
impana.r <impanar.bsc23@rvu.edu.in>
2024-01-30 19:47:56 +0530

    test

--- set01/problem09.c
+++ set01/problem09.c
@@ -1,6 +1,7 @@
 //. Write a C program to find the [square root](https://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Babylonian_method) of a number.
 
 #include <stdio.h>
+#include <math.h>
 
 float input()
 {
