96f8999dd37d3880bdc1e4bbd64a9746b019be16
impana.r <impanar.bsc23@rvu.edu.in>
2024-02-23 14:00:55 +0530

    test

--- set04/problem01.c
+++ set04/problem01.c
@@ -2,40 +2,34 @@
 
 #include <stdio.h>
 
-void input(int *num1, int *den1, int *num2, int *den2) {
-    printf("Enter the first fraction: ");
-    scanf("%d/%d", num1, den1);
+void input(int *num1, int *den1, int *num2, int *den2);
+void add(int num1, int den1, int num2, int den2, int *res_num, int *res_den);
+void output(int num1, int den1, int num2, int den2, int res_num, int res_den);
 
-    printf("Enter the second fraction: ");
-    scanf("%d/%d", num2, den2);
+int main() {
+    int n1, n2, d1, d2;
+    input(&n1, &n2, &d1, &d2);
+    int sum;
+    add(n1, n2, d1, d2, &sum);
+    output(n1, n2, d1, d2, sum);
+    return 0;
+}
+
+void input(int *num1, int *den1, int *num2, int *den2) {
+    printf("Enter the num1:");
+    scanf("%d", num1);
+    printf("Enter the den1:");
+    scanf("%d", den1);
+    printf("Enter the num2:");
+    scanf("%d", num2);
+    printf("Enter the den2:");
+    scanf("%d", den2);
 }
 
 void add(int num1, int den1, int num2, int den2, int *res_num, int *res_den) {
-    int lcm = (den1 * den2) / gcd(den1, den2);
-
-    *res_num = (num1 * lcm / den1) + (num2 * lcm / den2);
-    *res_den = lcm;
+    *res_num=num1/den1 + num2/den2;
 }
 
 void output(int num1, int den1, int num2, int den2, int res_num, int res_den) {
-    printf("Sum of the fractions %d/%d and %d/%d is %d/%d\n", num1, den1, num2, den2, res_num, res_den);
-}
-
-int gcd(int a, int b) {
-    while (b != 0) {
-        int temp = b;
-        b = a % b;
-        a = temp;
-    }
-    return a;
-}
-
-int main() {
-    int num1, den1, num2, den2, res_num, res_den;
-
-    input(&num1, &den1, &num2, &den2);
-    add(num1, den1, num2, den2, &res_num, &res_den);
-    output(num1, den1, num2, den2, res_num, res_den);
-
-    return 0;
+    printf("The sum of %d/%d and %d/%d is %d", num1, den1, num2, den2, res_num, res_den);
 }
