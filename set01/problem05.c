6466e44994f252a872acaf5f7fea15f471121a0d
impana.r <impanar.bsc23@rvu.edu.in>
2024-02-25 15:09:50 +0530

    test

--- set01/problem05.c
+++ set01/problem05.c
@@ -1,35 +1,35 @@
+//Write a C program to compare three numbers using **pass by value**
+
 #include <stdio.h>
 
-int input()
-{
-    int number;
-    printf("Enter the numbers:");
-    scanf("%d", &number);
-    return number;
+int input();
+int compare(int a, int b, int c);
+void output(int a, int b, int c, int largest);
 
-}
-
-int compare(int a, int b, int c)
-{
-    if(a>=b && a>=c)
-    return a;
-    else if(b>=c)
-    return b;
-    return c;
-}
-
-void output(int a, int b, int c, int largest)
-{
-    printf("The largest number of %d, %d, %d is %d", a, b, c, largest);
-}
-
-int main()
-{
+int main() {
     int a, b, c;
     a=input();
     b=input();
     c=input();
-    int large=compare(a, b, c);
-    output(a, b, c, large);
+    int largest;
+    largest=compare(a, b, c);
+    output(a, b, c, largest);
     return 0;
+}
+
+int input() {
+    int n;
+    printf("Enter the number:");
+    scanf("%d", &n);
+    return n;
+}
+
+int compare(int a, int b, int c) {
+    int largest;
+    largest=a>=b?:a>=c?:b>=c; //variable=expression1:expression2:expression3
+    return largest;
+}
+
+void output(int a, int b, int c, int largest) {
+    printf("The largest number of %d, %d and %d is %d", a, b, c, largest);
 }
