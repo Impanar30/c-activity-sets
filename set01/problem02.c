76f6c3ee91c2786dccec9e2d4680cdee8f330e02
impana.r <impanar.bsc23@rvu.edu.in>
2024-02-26 19:52:20 +0530

    test

--- set01/problem02.c
+++ set01/problem02.c
@@ -3,12 +3,8 @@
 #include <stdio.h>
 
 int main() {
-    int a, b;
-    printf("Enter the a value:");
-    scanf("%d", &a);
-    printf("Enter the b value:");
-    scanf("%d", &b);
-    int sum=a+b;
-    printf("%d", sum);
+    int a = 78, b = 56;
+    int sum = a + b;
+    printf("%d + %d = %d", sum);
     return 0;
 }
